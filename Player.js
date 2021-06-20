290598aa2d224ced6f7da4973fd0e828231b19d3
NamanRanka1 <78149561+NamanRanka1@users.noreply.github.com>
2021-06-20 14:49:54 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,60 @@
+class Player {
+constructor(){
+this.index = null;
+this.x=null;
+this.y=null;
+this.name = null;
+this.rank=null;
+}
+
+getCount(){
+var playerCountRef = database.ref('playerCount');
+playerCountRef.on("value",(data)=>{
+playerCount = data.val();
+})
+}
+
+updateCount(count){
+database.ref('/').update({
+playerCount: count
+});
+}
+
+update(){
+var playerIndex = "players/player" + this.index;
+database.ref(playerIndex).set({
+name:this.name,
+x:this.x,
+y:this.y
+});
+}
+
+static getPlayerInfo(){
+var playerInfoRef = database.ref('players');
+playerInfoRef.on("value",(data)=>{
+allPlayers = data.val();
+})
+}
+
+
+
+getRank(){
+
+var getRank=database.ref('carsAtEnd');
+getRank.on("value",(data)=>{
+this.rank=data.val();
+})
+
+}
+
+updateRank(rank){
+
+database.ref('/').update({
+carsAtEnd:rank
+})
+
+}
+
+
+}
+    
---
+++ sketch.js
@@ -0,0 +1,21 @@
+var fIMAGE,gIMAGE;
+var Fimage,Gimage;
+
+
+function preload(){
+
+fIMAGE=loadImage("images/fORMbg.png");
+gIMAGE=loadImage("images/gAMEbg.jpg");
+}
+  
+function setup(){
+
+createCanvas(displayWidth-170)
+
+}
+
+function draw(){
+
+
+
+}
